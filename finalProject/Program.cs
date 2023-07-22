4e1a64a03f7dbf7482264c73cdc511864358d364
firarustamli <firangizzr@code.edu.az>
2023-07-23 01:18:02 +0400

    structure-created,program class added

---
+++ finalProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace finalProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            int option;
+
+
+            do
+            {
+
+                Console.WriteLine("1. Operation on Products");
+                Console.WriteLine("2. Operation on Sales");
+                Console.WriteLine("3. Exit");
+               
+
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Enter an option please:");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid option!");
+                    Console.WriteLine("Enter an option please:");
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+
+
+                switch (option)
+                {
+                    case 1:
+                       
+                        break;
+                    case 2:
+                       
+                        break;
+                       
+                    case 3:
+                        Console.WriteLine("Good bye!");
+                        break;
+                    default:
+                        Console.WriteLine("There is no such option!");
+                        break;
+                }
+
+            } while (option != 0);
+        }
+    }
+}
---
+++ finalProject/Services/Concrete/MenuService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace finalProject.Services.Concrete
+{
+    public class MenuService
+    {
+        public static void OperationsOnProducts()
+        {
+
+        }
+
+
+        public static void OperationsOnSales()
+        {
+
+        }
+
+
+    }
+}
